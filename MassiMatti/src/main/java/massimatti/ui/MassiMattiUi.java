3ec0698c54b4cd4b6117e51a8d5e3593b70b6e49
InglouriousObjects <petri.toropainen.helsinki@gmail.com>
2019-11-15 14:17:17 +0200

    projektiin muutoksia

---
+++ MassiMatti/src/main/java/massimatti/ui/MassiMattiUi.java
@@ -0,0 +1,19 @@
+
+
+package massimatti.ui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author pjtoropa
+ */
+public class MassiMattiUi extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
