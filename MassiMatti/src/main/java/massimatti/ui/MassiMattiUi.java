5d6453e41a395c778cc61fb6ad2fd5bb5a31458c
InglouriousObjects <petri.toropainen.helsinki@gmail.com>
2019-11-15 22:33:54 +0200

    update

--- MassiMatti/src/main/java/massimatti/ui/MassiMattiUi.java
+++ MassiMatti/src/main/java/massimatti/ui/MassiMattiUi.java
@@ -3,6 +3,12 @@
 package massimatti.ui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -13,7 +19,22 @@ public class MassiMattiUi extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        VBox loginPane = new VBox(10);
+        HBox inputPane = new HBox(10);                                      //testin vuoksi copypaste todoApisa
+        loginPane.setPadding(new Insets(10));
+        Label loginLabel = new Label("username");
+        TextField usernameInput = new TextField();
+
+        inputPane.getChildren().addAll(loginLabel, usernameInput);
+        Label loginMessage = new Label();
+
+        Button loginButton = new Button("login");
+        Button createButton = new Button("create new user");
+    }
+    
+    
+    public static void main(String[] args) {
+        launch(args);
     }
     
 }
