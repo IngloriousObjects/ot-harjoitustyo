b97e97eb415492e2d1fc1aaa3730b00f5740d335
InglouriousObjects <petri.toropainen.helsinki@gmail.com>
2019-11-24 20:40:08 +0200

    updates to LoginView

--- MassiMatti/src/main/java/massimatti/ui/LoginView.java
+++ MassiMatti/src/main/java/massimatti/ui/LoginView.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 public class LoginView {
@@ -19,43 +20,71 @@ public class LoginView {
     }
 
     public Scene getLoginScene(Stage primaryStage) {
-        
+
         //Eriytetty kirjautumis/luokäyttäjä -näkymä
         // Koodi on vielä erittäin vajavainen...DAO..
-
         VBox loginPane = new VBox(10);
+
         VBox inputPane = new VBox(10);
         loginPane.setPadding(new Insets(10));
 
-        Label loginLabel = new Label("käyttäjätunnus");
+        Label loginLabel = new Label("Käyttäjätunnus");
         TextField usernameInput = new TextField();
-        Label loginLabelSecond = new Label("salasana");
+        Label loginLabelSecond = new Label("Salasana");
         PasswordField password = new PasswordField();
 
         inputPane.getChildren().addAll(loginLabel, usernameInput, loginLabelSecond, password);
-        Label loginMessage = new Label();
 
-        Button loginButton = new Button("kirjaudu");
-        Button createButton = new Button("rekisteröidy");
+        Label loginMessage = new Label("KIRJAUDU");
+        Label registerMessage = new Label("REKISTERÖIDY");
+        TextFlow flow = new TextFlow();
+        loginMessage.setStyle("-fx-font-weight: bold");
+        registerMessage.setStyle("-fx-font-weight: bold");
+        flow.getChildren().addAll(loginMessage, registerMessage);
+        Button loginButton = new Button("Kirjaudu");
+        Button registerButton = new Button("Rekisteröidy");
+        Button registerUserButton = new Button("Rekisteröidy");
+        Button backToLoginButton = new Button("Palaa");
 
-        loginPane.getChildren().addAll(loginMessage, inputPane, loginButton, createButton);
+        loginPane.getChildren().addAll(loginMessage, inputPane, loginButton, registerButton);
 
         Scene loginScene = new Scene(loginPane, 280, 260);
-        Scene create = new Scene(loginPane, 280, 260);
-        
 
+        registerButton.setOnAction((event) -> {
+
+            changeToRegisterView(loginPane, loginMessage, registerUserButton, backToLoginButton, loginButton, registerButton);
+
+        });
+        
+        backToLoginButton.setOnAction((event) -> {
+
+            backToLoginView(loginPane, loginMessage, registerUserButton, backToLoginButton, loginButton, registerButton);
+
+        });
+        
+        
         return loginScene;
 
     }
 
-        
-    public Scene getCreateUserScene(Scene create){
-        
-        
-        
-        
-        
-        
-        
+    public void changeToRegisterView(VBox login, Label loginMessage, Button registerUser, Button back, Button loginButton, Button registerButton) {
+
+        login.getChildren().remove(loginButton);
+        login.getChildren().remove(registerButton);
+        login.getChildren().add(registerUser);
+        login.getChildren().add(back);
+        loginMessage.setText("REKISTERÖIDY");
+
     }
+
+    public void backToLoginView(VBox login, Label loginMessage, Button registerUser, Button back, Button loginButton, Button registerButton) {
+
+        login.getChildren().remove(registerUser);
+        login.getChildren().remove(back);
+        login.getChildren().add(loginButton);
+        login.getChildren().add(registerButton);
+        loginMessage.setText("KIRJAUDU");
+
+    }
+
 }
