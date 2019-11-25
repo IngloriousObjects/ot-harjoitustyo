6ab994b2af92d9df8607379d6b4357c8be88b522
InglouriousObjects <petri.toropainen.helsinki@gmail.com>
2019-11-25 21:39:42 +0200

    more classes

---
+++ MassiMatti/src/main/java/massimatti/dao/DatabaseUserDao.java
@@ -0,0 +1,22 @@
+
+package massimatti.dao;
+
+import massimatti.domain.User;
+
+/**
+ *
+ * 
+ */
+public class DatabaseUserDao implements UserDao {
+
+    @Override
+    public User create(User user) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public User findByUsernameAndPassword(String username, String password) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
