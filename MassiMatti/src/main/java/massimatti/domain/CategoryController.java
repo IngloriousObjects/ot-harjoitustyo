daf8f0e971aa120ac7ebf3add4911b7696fcd3c9
InglouriousObjects <petri.toropainen.helsinki@gmail.com>
2019-11-26 14:34:32 +0200

    updates

--- MassiMatti/src/main/java/massimatti/domain/CategoryController.java
+++ MassiMatti/src/main/java/massimatti/domain/CategoryController.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package massimatti.dao;
+package massimatti.domain;
 
 /**
  *
  * @author pjtoropa
  */
-public interface MassiMattiDao {
-
+public class CategoryController {
+    
 }
